d0756344388dabe8a34460337fff7db6cb501adc
Nihar Hegde <niharhegde163@gmail.com>
2023-08-06 16:37:30 +0530

    fixing small bug and storing data in mongodb

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -5,11 +5,6 @@ import { connectToDb } from "@utils/database";
 
 import User from "@models/users";
 
-console.log({
-  clientId: process.env.GOOGLE_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-});
-
 const handler = NextAuth({
   providers: [
     GoogleProvier({
@@ -17,33 +12,35 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    });
-    session.user.id = sessionUser._id.toString();
-    return session;
-  },
-  async signIn({ profile }) {
-    try {
-      await connectToDb();
-      //check if user already exists?
-      const userExists = await User.findOne({ email: profile.email });
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
+      });
+      session.user.id = sessionUser._id.toString();
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDb();
+        //check if user already exists?
+        const userExists = await User.findOne({ email: profile.email });
 
-      //if not create a new user
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
-        });
+        //if not create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log("error while sighing in", error);
+        return false;
       }
-
-      return true;
-    } catch (error) {
-      console.log("error while sighing in", error);
-      return false;
-    }
+    },
   },
 });
 export { handler as GET, handler as POST };
