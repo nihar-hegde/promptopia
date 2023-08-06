7660a3b2eee97a09a99ccf3f7ea652db1a3f5c60
Nihar Hegde <niharhegde163@gmail.com>
2023-08-06 19:01:10 +0530

    creating feeds card

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -2,9 +2,32 @@
 import { useState, useEffect } from "react";
 import PromptCard from "./PromptCard";
 
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
+
 const Feed = () => {
   const [searchText, setSearchText] = useState("");
+  const [posts, setPost] = useState([]);
   const handleSearchChange = (e) => {};
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+      setPost(data);
+    };
+    fetchPosts();
+  }, []);
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
@@ -17,6 +40,7 @@ const Feed = () => {
           className="search_input peer"
         />
       </form>
+      <PromptCardList data={posts} handleTagClick={() => {}} />
     </section>
   );
 };
